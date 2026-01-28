d1d3e11d92cf5fff0e0fc0a153bf644ad3e5eabc
unknown <benjamin.tyler@mymail.champlain.edu>
2026-01-28 14:22:14 -0500

    commit for week 3

---
+++ week3/functions_and_event_logs.ps1
@@ -0,0 +1,23 @@
+﻿#Get-EventLog System -Source Microsoft-Windows-Winlogon
+
+$loginouts = Get-EventLog System -Source Microsoft-Windows-Winlogon -After (Get-Date).AddDays(-14)
+
+$loginoutsTable = @()
+for($i=0; $i -lt $loginouts.Count; $i++){
+
+$event = ""
+if($loginouts[$i].InstanceId -eq 7001) {$event="Logon"}
+if($loginouts[$i].InstanceId -eq 7002) {$event="Logon"}
+
+$user = $loginouts[$i].ReplacementStrings[1]
+$SID = New-Object System.Security.Principal.SecurityIdentifier($user)
+$new_user = $SID.Translate([System.Security.Principal.NTAccount])
+
+$loginoutsTable += [pscustomobject]@{"Time" = $loginouts[$i].TimeGenerated        
+                          "ID" = $loginouts[$i].InstanceId
+             "Event" = $event;
+       "User" = $new_user.Value
+
+}
+}
+$loginoutsTable
