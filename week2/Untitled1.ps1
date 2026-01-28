2a063828851b0160cd701873d2fdba817e4be258
unknown <benjamin.tyler@mymail.champlain.edu>
2026-01-28 12:23:05 -0500

    commit for week 2

---
+++ week2/Untitled1.ps1
@@ -0,0 +1,38 @@
+﻿#(Get-NetIPAddress -AddressFamily IPv4 | Where-Object {$_.InterfaceAlias -ilike "Ethernet"}).IPAddress
+
+#(Get-NetIPAddress -AddressFamily IPv4 | Where-Object {$_.InterfaceAlias -ilike "Ethernet"}).PrefixLength
+
+#Get-WmiObject -List | Where-Object { $_.Name -ilike "Win32_Net*" } | Sort-Object Name
+
+#Get-CimInstance Win32_NetworkAdapterConfiguration -Filter "DHCPEnabled='True'" | select DHCPServer | Format-Table -HideTableHeaders
+
+#(Get-DnsClientServerAddress -AddressFamily IPv4 | Where-Object { $_.InterfaceAlias -ilike "*Ethernet*" } ).ServerAddresses[0]
+
+#cd $PSScriptRoot
+
+#$files=(Get-ChildItem)
+#for ($j=0; $j -le $files.count; $j++){
+
+#    if ($files[$j].name -ilike "*ps1"){
+#        Write-Host $files[$j].name
+#    }
+#}
+
+#$folderpath="$PSScriptRoot\outfolder"
+#if (Test-Path $folderpath){
+#    Write-Host "Folder Already Exists"
+#}
+#else {New-Item -Path $folderpath}
+
+cd $PSScriptRoot
+$files=(Get-ChildItem)
+
+$folderpath = "$PSScriptRoot/outfolder/"
+$filePath = Join-Path $folderpath "out.csv"
+
+$files | Where-Object { $_.Extension -eq ".ps1" } | `
+Export-Csv -Path $filePath
+
+#$files = Get-ChildItem -Recurse -File
+#$files | Where-Object { $_.Extenstion -eq '.csv' } | Rename-Item -NewName {$_.Name -replace '.csv', '.log'}
+#Get-ChildItem -Recurse -File
---
+++ week2/process_management_1.ps1
@@ -0,0 +1,8 @@
+﻿if (Get-Process | Where-Object ProcessName -eq "chrome" ) {
+    if (Get-Service | Where-Object {$_.status -eq "Running"} ) {
+        Stop-Process -Name "Chrome"
+    }
+}
+else{
+Start-Process chrome.exe -ArgumentList "https://www.champlain.edu"
+}
