6bc3201747e78391079722ca35a04e1787fc5192
unknown <benjamin.tyler@mymail.champlain.edu>
2026-02-04 14:13:41 -0500

    commit for windows apache logs

---
+++ week4/windowsapachelogs.ps1
@@ -0,0 +1,19 @@
+﻿#Deliverable 2:
+#Get-Content -Path "C:\xampp\apache\logs\access.log" -Tail 5
+
+#Deliverbale 3:
+#Get-Content C:\xampp\apache\logs\access.log | Select-String ' 404 '
+
+#Deliverbale 4:
+#Get-Content C:\xampp\apache\logs\access.log | Select-String ' 200 ' -NotMatch
+
+#Deliverable 5:
+#Select-String -Path ./*.log -Pattern 'error'
+
+#Deliverable 6:
+#$ip = Get-Content C:\xampp\apache\logs\access.log | Select-String ' 404 '
+#Write-Output $ip.ForEach({( $_ -split "\s+" )[0]})
+
+#Deliverable 7:
+$ip = Get-Content C:\xampp\apache\logs\access.log | Select-String ' 404 '
+Write-Output $ip.ForEach({( $_ -split "\s+" )[0]}) | Group-Object -NoElement
