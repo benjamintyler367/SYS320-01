e2f1c2196061e71c5b8ce541f861dc318c53542d
unknown <benjamin.tyler@mymail.champlain.edu>
2026-02-17 21:38:42 -0500

    week 5 a1 finished

--- week5/Scraping_Champlain_Classes.ps1
+++ week5/Scraping_Champlain_Classes.ps1
@@ -1,1 +1,61 @@
 ﻿function gatherClasses(){
+
+$page = Invoke-WebRequest -TimeoutSec 2 http://10.0.17.24/courses2026sp.html
+
+#get all the tr elements of html documents
+$trs = $page.ParsedHTML.body.getElementsByTagName("tr")
+
+#empty arry to hold results
+$FullTable = @()
+for($i=1; $i -lt $trs.length; $i++){ #going over the tr element
+
+    #get every td element of current tr element
+    $tds = $trs[$i].getElementsByTagName("td")
+        
+    #want to separate start time and end time from one time field
+    $Times = $tds[5].innerText.Split("-")
+
+    $FullTable += [pscustomobject]@{"Class Code" = $tds[0].innerText; `
+                                    "Title"      = $tds[1].innerText; `
+                                    "Days"       = $tds[4].innerText; `
+                                    "Time Start" = $Times[0]; `
+                                    "TIme End"   = $Times[1]; `
+                                    "Instructor" = $tds[6].innerText; `
+                                    "Location"   = $tds[9].innerText;`
+                                }
+
+} # end of the loop
+return $FullTable
+} 
+
+gatherClasses
+
+function daysTranslator($FullTable){
+
+# Go over every record in the table
+for($i=0; $i -lt $FullTable.length; $i++){
+
+    # Empty array to hold days for every record
+    $Days = @()
+
+    # If you see "M" -> Monday
+    if($FullTable[$i].Days -ilike "*M*"){  $Days += "Monday"  }
+
+    # If you see "T" followed by T, W, or F -> Tuesday
+    if($FullTable[$i].Days -ilike "*T[W,F]*"){  $Days += "Tuesday"  }
+
+    # If you see a "W" -> Wednesday
+    if($FullTable[$i].Days -ilike "*W*"){  $Days += "Wednesday"  }
+
+    # If you see a "Th" -> Thursday
+    if($FullTable[$i].Days -ilike "*TH*"){  $Days += "Thursday"  }
+
+    # If you see a "F" -> Friday
+    if($FullTable[$i].Days -ilike "*F*"){  $Days += "Friday"  }
+
+    # Make the Switch
+    $FullTable[$i].Days = $Days
+
+}
+return $FullTable
+}
---
+++ week5/Scraping_Champlain_Classes_2.ps1
@@ -0,0 +1,29 @@
+﻿. "C:\Users\champuser\SYS320-01\week5\Scraping_Champlain_Classes.ps1"
+$rawClasses = gatherClasses
+$classes = daysTranslator $rawClasses
+#deliverable 1
+#$classes | Where-Object {$_.Instructor -eq "Furkan Paligu"}
+
+<#deliverable 2
+pscustomobject
+
+$classes | Where-Object {($_.Location -eq "FREE 105") -and ($_.days -eq "Wednesday")} | `
+    Sort-Object "Time Start" | `
+    Select-Object "Time Start", "Time End", "Class Code"
+#>
+
+<#Deliveranle 3 and 4
+
+$its_instructors = $classes | Where-Object {
+                             ($_."Class Code" -like "SYS*") -or `
+                             ($_."Class Code" -like "SEC*") -or `
+                             ($_."Class Code" -like "NET*") -or `
+                             ($_."Class Code" -like "FOR*") -or `
+                             ($_."Class Code" -like "CSI*") -or `
+                             ($_."Class Code" -like "DAT*") } `
+                             | Sort-Object "Instructor" -Unique `
+                             | Select-Object -ExpandProperty "Instructor"
+                             $its_instructors
+                             #>
+$classes | Where-Object {$_.Instructor -in $its_instructors } `
+         | Group-Object "Instructor" | Select-Object Count, Name | Sort-Object Count -Descending
