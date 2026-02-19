4f5d1ad6e09c05c86fb9500b0f41eb48c97f0b92
unknown <benjamin.tyler@mymail.champlain.edu>
2026-02-18 21:01:33 -0500

    commit for 5_4

---
+++ week5/htmlelementsassignment.ps1
@@ -0,0 +1,16 @@
+﻿$scraped_page = Invoke-WebRequest -TimeoutSec 10 http://10.0.17.24/ToBeScraped.html
+
+# Get a count of the links in the page
+#$scraped_page.Links.Count
+
+#$scraped_page.Links
+
+#$scraped_page.Links | Select-Object outerText, href
+
+#$h2s=$scraped_page.ParsedHtml.body.getElementsByTagName("h2") | select outerText
+#$h2s
+
+$divs1=$scraped_page.ParsedHtml.body.getElementsByTagName("div") | where { `
+$_.getAttributeNode("class").value -ilike "div-1" } | select innerText
+
+$divs1
