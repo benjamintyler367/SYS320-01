16af440b3aa7cc9f9d80b8d4cd59287b57da4710
root <benjamin.tyler@mymail.champlain.edu>
2026-04-21 15:52:56 -0400

    commit for 4 21

---
+++ week12/networkchecker.bash
@@ -0,0 +1,105 @@
+#!/bin/bash
+
+myIP=$(ip addr | grep "/24 brd" | grep -o -E "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" | grep -v ".255")
+
+# Todo-1: Create a helpmenu function that prints help for the script
+function HelpMenu(){
+	echo -e "\n"
+	echo "  HELP MENU  "
+	echo "-------------"
+	echo "-n: Add -n as an argument for this script to use nmap"
+	echo " -n external: External NMAP scan"
+	echo " -n internal: Internal NMAP scan"
+	echo "-s: Add -s as an argument for this script to use ss"
+	echo " -s external: External ss(NetStat) scan"
+	echo " -s internal: Internal ss(NetStat) scan"
+	echo -e "\n"
+
+	exit 1
+}
+
+
+# Return ports that are serving to the network
+function ExternalNmap(){
+  rex=$(nmap "${myIP}" | awk -F"[/[:space:]]+" '/open/ {print $1,$4}' )
+  echo "$rex"
+}
+
+# Return ports that are serving to localhost
+function InternalNmap(){
+  rin=$(nmap localhost | awk -F"[/[:space:]]+" '/open/ {print $1,$4}' )
+  echo "$rin"
+}
+
+
+# Only IPv4 ports listening from network
+function ExternalListeningPorts(){
+# Todo-2: Complete the ExternalListeningPorts that will print the port and application
+# that is listening on that port from network (using ss utility)
+	expo=$(ss -ltpn4 | awk  -F'[[:space:]:(),]+' '!/127.0.0./ && !/Address/ {print $5,$9}' | tr -d '"')
+	echo "$expo"
+}
+
+
+# Only IPv4 ports listening from localhost
+function InternalListeningPorts(){
+	ilpo=$(ss -ltpn | awk  -F"[[:space:]:(),]+" '/127.0.0./ {print $5,$9}' | tr -d "\"")
+	echo "$ilpo"
+}
+
+
+
+# Todo-3: If the program is not taking exactly 2 arguments, print helpmen
+
+if [[ $# -ne 2 ]]; then
+	echo "Invalid amount of arguments!"
+	HelpMenu
+fi
+
+# Todo-4: Use getopts to accept options -n and -s (both will have an argument)
+# If the argument is not internal or external, call helpmenu
+# If an option other then -n or -s is given, call helpmenu
+# If the options and arguments are given correctly, call corresponding functions
+# For instance: -n internal => will call NMAP on localhost
+#               -s external => will call ss on network (non-local)
+
+while getopts ":n:s:" opt; do
+	case "$opt" in
+		n)
+			case "$OPTARG" in
+				internal)
+					InternalNmap
+					;;
+				external)
+					ExternalNmap
+					;;
+				*)
+					echo "Invalid argument for -n: $OPTARG"
+					HelpMenu
+					;;
+			esac
+			;;
+		s)
+			case "$OPTARG" in
+				internal)
+					InternalListeningPorts
+					;;
+				external)
+					ExternalListeningPorts
+					;;
+				*)
+					echo "Invalid argument for -s: $OPTARG"
+					HelpMenu
+					;;
+			esac
+			;;
+		\?)
+			echo "Invalid option: -$OPTARG"
+			HelpMenu
+			;;
+		:)
+			echo "Option -$OPTARG requires an argument"
+			HelpMenu
+			;;
+	esac
+done
